c560fcd78817b15f867d1452372f8edccd57b93d
joneirikboska <joneirikboska@gmail.com>
2021-04-05 09:57:51 +0200

    Initial commit. Set up basic socket.io server.

---
+++ server/app.js
@@ -0,0 +1,29 @@
+const app = require('express')()
+const http = require('http').Server(app)
+const io = require('socket.io')(http)
+const index = require('./routes/index')
+
+const port = process.env.PORT || 4001
+
+app.use(index)
+
+let interval
+
+io.on('connection', socket => {
+  console.log('New client connected')
+  if (interval) {
+    clearInterval(interval)
+  }
+  interval = setInterval(() => getApiAndEmit(socket), 1000)
+  socket.on('disconnect', () => {
+    console.log('Client disconnected')
+    clearInterval(interval)
+  })
+})
+
+const getApiAndEmit = socket => {
+  const response = new Date()
+  socket.emit('FromAPI', response)
+}
+
+http.listen(port, () => console.log(`Listening on port ${port}`))
