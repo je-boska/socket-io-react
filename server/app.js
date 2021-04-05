53af8e7124769e25499a6b4533e19561558c3007
joneirikboska <joneirikboska@gmail.com>
2021-04-05 10:22:21 +0200

    Create React frontend which displays moment formatted date from socket connection to server.

--- server/app.js
+++ server/app.js
@@ -1,6 +1,11 @@
 const app = require('express')()
 const http = require('http').Server(app)
-const io = require('socket.io')(http)
+const io = require('socket.io')(http, {
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
+  },
+})
 const index = require('./routes/index')
 
 const port = process.env.PORT || 4001
